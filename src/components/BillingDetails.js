62f2228c99d8b04962199dfb9b315966f561f606
PrafulShinde8 <126104190+PrafulShinde8@users.noreply.github.com>
2024-06-28 20:46:05 +0530

    Update BillingDetails.js

---
+++ src/components/BillingDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BillingDetails = ({ billing }) => (
+  <div>
+    <h2>Billing Details</h2>
+    <p>Aditya Nadkarni</p>
+    <p>A2/302, Adarsh Building, Thane W </p>
+    <p>Thane, Maharashtra, 400610</p>
+    <p>State/UT Code: 27</p>
+  </div>
+);
+
+export default BillingDetails;
